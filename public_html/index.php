7e389363aab1476064117b6c9c508e16ca9f9f2c
teticio@gmail.com <teticio@gmail.com>
2019-11-21 19:18:23 +0100

    ok

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,4 @@
 <?php
-    // TODO:
-    // refactor common functions into utils
-    // upload urltovec.p and bandcamp_tracks.p to drive, download automatically
-    // rotate screen in app?
-    // cookies
-    
     require '../vendor/autoload.php';
 
     $session = new SpotifyWebAPI\Session(
